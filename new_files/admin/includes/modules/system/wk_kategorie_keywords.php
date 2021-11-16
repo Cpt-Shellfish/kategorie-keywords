593539496689f9ef04627427dafaad00912f2e43
Cpt-Shellfish <github@luziferase.de>
2021-11-16 16:42:41 +0100

    add lost changes

--- new_files/admin/includes/modules/system/wk_kategorie_keywords.php
+++ new_files/admin/includes/modules/system/wk_kategorie_keywords.php
@@ -40,22 +40,27 @@ class wk_kategorie_keywords extends StdModule
         
         foreach ($this->arr_files as $template => $data) {
           for($i=0; $i < count($data); $i++){     
-              $mmlc_tpl = str_replace("/", "/wkkk_", $data[$i]["tpl"]); 
-              $tpl_dest = "templates/" . $template  . "/" . $mmlc_tpl;                       
+              #$mmlc_tpl = str_replace("/", "/wkkk_", $data[$i]["tpl"]); 
+              #$tpl_dest = "templates/" . $template  . "/" . $mmlc_tpl;                       
               $tpl_src = "templates/" . $template . "/" . $data[$i]["tpl"];
               if(is_file(DIR_FS_CATALOG . $tpl_src)) {
+                 $this->text_return .= '<br /><i>' . $tpl_src;
                  $checksum_src = md5(file_get_contents(DIR_FS_CATALOG . $tpl_src));  
                  if($checksum_src == $data[$i]["checksum_dest"]) {
                     # bereits passend modifiziert
+                    $this->text_return .= ' **';
                  }elseif($checksum_src == $data[$i]["checksum_src"]) {
                    # nicht modifiziert und original
-                   $this->text_return .= '<br /><i>' . $tpl_src . ' *</i>';
+                   $this->text_return .= ' *';
                  }  
+                 $this->text_return .= '</i>';
+                 /*
                  if(is_file(DIR_FS_CATALOG . $tpl_dest)) {
                    # Vorlagen vorhanden
                    #$this->text_mod .= '<br /><i><a href="../' . $tpl_dest . '">' .  $tpl_dest . '</a></i>';
                    $this->text_mod .= '<br /><i>' .  $tpl_dest . '</i>';
-                 }                              
+                 }  
+                 */                            
               }                    
           }                         
         }    
@@ -66,9 +71,10 @@ class wk_kategorie_keywords extends StdModule
           $this->text_return =  MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_TPL_MOD . $this->text_return;
           $this->text_return .=  MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_TPL_ORIG;  
         }
-        if($this->text_mod != ""){
-          $this->text_return .=  $this->text_mod;
-        } 
+        $this->text_return .= '<a href="../wkkk_geaendert.zip">wkkk_geaendert.zip</a><br />';
+        #if($this->text_mod != ""){
+        #  $this->text_return .=  $this->text_mod;
+        #} 
         
         $this->text_return .=  '<br /><div align="center">' . xtc_button(BUTTON_SAVE) .
           xtc_button_link(BUTTON_CANCEL, xtc_href_link(FILENAME_MODULE_EXPORT, 'set=' . $_GET['set'] . '&module=' . $this->code)) . 
@@ -103,89 +109,4 @@ class wk_kategorie_keywords extends StdModule
     {
         parent::remove();
     }
-}
-
-
-
-/*
-class hp_adminer_dba_tool extends StdModule
-{
-    public function __construct()
-    {
-        $this->init('MODULE_HP_ADMINER_DBA_TOOL');
-    }
-
-    public function display()
-    {
-        return array('text' => '<br /><div align="center">' . xtc_button(BUTTON_SAVE) .
-        xtc_button_link(BUTTON_CANCEL, xtc_href_link(FILENAME_MODULE_EXPORT, 'set=' . $_GET['set'] . '&module=' . $this->code)) . "</div>");
-    }
-
-    public function install()
-    {
-        xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, date_added)
-              				VALUES ('MODULE_HP_ADMINER_DBA_TOOL_STATUS', 'true',  '6', '1', 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");
-        xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, date_added)
-              				VALUES ('MODULE_HP_ADMINER_DBA_TOOL_FILL_LOGIN', 'true',  '6', '2', 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");
-        xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, date_added)
-              				VALUES ('MODULE_HP_ADMINER_DBA_TOOL_LOG_SQL', 'true',  '6', '3', 'xtc_cfg_select_option(array(\'true\', \'false\'), ', now())");  
-        xtc_db_query("INSERT INTO " . TABLE_CONFIGURATION . " (configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, date_added)
-              				VALUES ('MODULE_HP_ADMINER_DBA_TOOL_PASSWORD', '',  '6', '4', 'xtc_cfg_password_field_module(', now())");                                                                    
-
-        xtc_db_query("ALTER TABLE `" . TABLE_ADMIN_ACCESS . "` ADD `" . $this->code . "` INT(1) NOT NULL DEFAULT '0'");
-        xtc_db_query("UPDATE `" . TABLE_ADMIN_ACCESS . "` SET `" . $this->code . "` = '1' WHERE customers_id = '1'");
-        
-        require_once(DIR_FS_INC . 'xtc_random_charcode.inc.php');
-        
-        $random_charcode_1 = xtc_random_charcode(10);
-        $random_charcode_2 = xtc_random_charcode(10);
-        
-        rename(DIR_FS_CATALOG . DIR_ADMIN . (defined('DIR_ADMINER') ? DIR_ADMINER : 'adminer') . '/' . (defined('ADMINER_INDEX_FILE') ? ADMINER_INDEX_FILE : 'index.php'), DIR_FS_CATALOG . DIR_ADMIN . (defined('DIR_ADMINER') ? DIR_ADMINER : 'adminer') . '/index_' . $random_charcode_2 . '.php');        
-        rename(DIR_FS_CATALOG . DIR_ADMIN . (defined('DIR_ADMINER') ? DIR_ADMINER : 'adminer'), DIR_FS_CATALOG . DIR_ADMIN . 'adminer_' . $random_charcode_1);
-                                            
-        $file_contents = '<?php' . "\n" .                    
-                         '' . "\n" .
-                         'define(\'ADMINER_DBA_TOOL_TOKEN\', \'' . xtc_random_charcode(10) . '\');' . "\n" .
-                         'define(\'ADMINER_DBA_TOOL_LOG_SQL\', \'true\');' . "\n" .
-                         'define(\'DIR_ADMINER\', \'adminer_' . $random_charcode_1 . '\');' . "\n" .
-                         'define(\'ADMINER_INDEX_FILE\', \'index_' . $random_charcode_2 . '.php\');';                             
-                        
-        $fp = fopen(DIR_FS_CATALOG . 'includes/extra/configure/hp_adminer_dba_tool.php', 'w');
-        fputs($fp, $file_contents);
-        fclose($fp);    
-        @chmod(DIR_FS_CATALOG . 'includes/extra/configure/hp_adminer_dba_tool.php', 0644);
-    }
-
-    public function remove()
-    {
-        xtc_db_query("DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key LIKE 'MODULE_HP_ADMINER_DBA_TOOL_%'");
-      
-        $query_result = xtc_db_query("SHOW COLUMNS FROM `" . TABLE_ADMIN_ACCESS . "`");
-        $db_table_rows = [];
-        while ($row = xtc_db_fetch_array($query_result)) {
-          $db_table_rows[] = $row['Field'];
-        }
-        
-        if (in_array($this->code, $db_table_rows)) {
-          xtc_db_query("ALTER TABLE `" . TABLE_ADMIN_ACCESS . "` DROP `" . $this->code . "`;");
-        }      
-                
-        if (defined('DIR_ADMINER') && defined('ADMINER_INDEX_FILE')) { 
-        
-          rename(DIR_FS_CATALOG . DIR_ADMIN . DIR_ADMINER . '/' . ADMINER_INDEX_FILE, DIR_FS_CATALOG . DIR_ADMIN . DIR_ADMINER . '/index.php');        
-          rename(DIR_FS_CATALOG . DIR_ADMIN . DIR_ADMINER , DIR_FS_CATALOG . DIR_ADMIN . 'adminer');        
-                                    
-          $fp = fopen(DIR_FS_CATALOG . 'includes/extra/configure/hp_adminer_dba_tool.php', 'w');
-          fputs($fp, '<?php');
-          fclose($fp);    
-          @chmod(DIR_FS_CATALOG . 'includes/extra/configure/hp_adminer_dba_tool.php', 0644);                             
-       }
-    }
-    
-    public function keys() 
-    {
-        return ['MODULE_HP_ADMINER_DBA_TOOL_STATUS','MODULE_HP_ADMINER_DBA_TOOL_FILL_LOGIN','MODULE_HP_ADMINER_DBA_TOOL_LOG_SQL','MODULE_HP_ADMINER_DBA_TOOL_PASSWORD'];
-    }    
-}
-
-*/
+}        
--- new_files/lang/english/extra/admin/wk_kategorie_keywords.php
+++ new_files/lang/english/extra/admin/wk_kategorie_keywords.php
@@ -6,15 +6,17 @@
    ---------------------------------------------------------------------------------------*/
    
 define('MODULE_WK_KATEGORIE_KEYWORDS_TEXT_TITLE', 'Kategorie-Keywords');
-define('MODULE_WK_KATEGORIE_KEYWORDS_TEXT_DESCRIPTION', '<strong>Shopsuche: Erg&auml;nzung von Kategorie-Keywords</strong><br />Ist der Suchbegriff Bestandteil von Kategorie-Keywords werden zus&auml;tzlich zum regul&auml;ren Suchergebnis alle Artikel der betreffenden Kategorie gelistet. Optional werden ausserdem  die erfassten Kategorien oberhalb des Suchergebnisses platziert. <br />Bei bereits gew&auml;hlter Kategorie in der erweiterten Suche erfolgt kein Abgleich der Kategorie-Keywords.');
+define('MODULE_WK_KATEGORIE_KEYWORDS_TEXT_DESCRIPTION', '<br />Ist der Suchbegriff Bestandteil von Kategorie-Keywords werden zus&auml;tzlich zum regul&auml;ren Suchergebnis alle Artikel der betreffenden Kategorie gelistet. Optional werden ausserdem  die erfassten Kategorien oberhalb des Suchergebnisses platziert. <br />Bei bereits gew&auml;hlter Kategorie in der erweiterten Suche erfolgt kein Abgleich der Kategorie-Keywords.<br />');
 define('MODULE_WK_KATEGORIE_KEYWORDS_TEXT_INFO', MODULE_WK_KATEGORIE_KEYWORDS_TEXT_DESCRIPTION);
+define('MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_TPL_MOD', '<br /><strong>Hinweis - F&uuml;r folgende Funktionen sind unten genannte Template Anpassungen notwendig:</strong><br />- Anpassung der &Uuml;berschrift im Kategorie Listing Suchergebnis zu "gefunden in folgenden Kategorien:"<br />- Anzeige des Suchergebnisses in der Vorschau der Autocomplete Suche<br />');
+define('MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_TPL_ORIG', '<br /><br />Die mit ** gekennzeichneten Dateien sind bereits passend modifiziert.<br />Die mit * gekennzeichneten Dateien liegen im Original vor und k&ouml;nnen mit dem Inhalt der modifizierten Versionen &uuml;berschrieben werden (alternativ: Umbenennen von Original und modifizierter Version). Anderenfalls sind die Anpassungen per Dateivergleich zu erg&auml;nzen:<br /> ');
 
 define('MODULE_WK_KATEGORIE_KEYWORDS_STATUS_TITLE', 'Modul aktivieren?');
-define('MODULE_WK_KATEGORIE_KEYWORDS_STATUS_DESC', '');
+define('MODULE_WK_KATEGORIE_KEYWORDS_STATUS_DESC', MODULE_WK_KATEGORIE_KEYWORDS_TEXT_DESCRIPTION);
 define('MODULE_WK_KATEGORIE_KEYWORDS_SHOW_TITLE', 'Kategorie-Listing anzeigen?');
-define('MODULE_WK_KATEGORIE_KEYWORDS_SHOW_DESC', 'Listing der erfassten Kategorien &uuml;ber dem Suchergebnis');
+define('MODULE_WK_KATEGORIE_KEYWORDS_SHOW_DESC', 'Listing der erfassten Kategorien &uuml;ber dem Suchergebnis<br />');
 define('MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_CATEGORIES_TITLE', 'Kategorienamen als Keyword verwenden');
-define('MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_CATEGORIES_DESC', '');
+define('MODULE_WK_KATEGORIE_KEYWORDS_INCLUDE_CATEGORIES_DESC', ' ');
 
 define('MODULE_WK_KATEGORIE_KEYWORDS_TEXT_CATEGORY_KEYWORDS', 'Keywords f&uuml;r die Suche (kommagetrennt):');
 
